83cedac1ccc639e3444ee271e76a358ab6be11b8
Martin Simecek <simecek.m@seznam.cz>
2019-05-27 21:34:28 +0200

    add error component

---
+++ src/component/Error.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Error.sass'
+import errorImg from '../assets/image/404.svg';
+
+function Error(){
+	return (
+		<div>
+			<h1 class="title">Oooooooops</h1>
+			<img src={errorImg} alt="error"/>
+			<p class="text-primary">Something went wrong!</p>
+			<p class="text-secondary">Please try again later...</p>
+		</div>
+	)
+}
+
+export default Error;
