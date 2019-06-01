b04210a08b8d17b5b10e8f934f74386e5dd752bd
Martin Šimeček <simecek.m@seznam.cz>
2019-06-01 18:55:57 +0200

    use className in JSX

--- src/component/Error.js
+++ src/component/Error.js
@@ -3,13 +3,14 @@ import React from 'react';
 import './Error.sass'
 import errorImg from '../assets/image/404.svg';
 
-function Error(){
+function Error(props){
+  console.log(props);  
   return (
     <div>
-      <h1 class="title">Oooooooops</h1>
+      <h1 className="title">Oooooooops</h1>
       <img src={errorImg} alt="error"/>
-      <p class="text-primary">Something went wrong!</p>
-      <p class="text-secondary">Please try again later...</p>
+      <p className="text-primary">Something went wrong!</p>
+      <p className="text-secondary">Please try again later...</p>
     </div>
   )
 }
--- src/component/TodoListWidget.js
+++ src/component/TodoListWidget.js
@@ -5,12 +5,12 @@ import trash from '../assets/image/trash.svg'
 
 function TodoListWidget({ title, count }){
   return (
-    <div class="container">
-      <div class="list-item">
-        <span class="list-title">{ title }</span>
-        <span class="list-count">({ count })</span>
+    <div className="container">
+      <div className="list-item">
+        <span className="list-title">{ title }</span>
+        <span className="list-count">({ count })</span>
       </div>
-      <img class="icon-trash" src={ trash } alt="trash"/>
+      <img className="icon-trash" src={ trash } alt="trash"/>
     </div>
   )
 }
