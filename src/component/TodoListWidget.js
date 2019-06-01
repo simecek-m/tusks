9def97797cca9105f67ca1e487bffa11fb6bad55
Martin Simecek <simecek.m@seznam.cz>
2019-06-01 16:40:41 +0200

    refactor indent size

--- src/component/TodoListWidget.js
+++ src/component/TodoListWidget.js
@@ -4,15 +4,15 @@ import './TodoListWidget.sass';
 import trash from '../assets/image/trash.svg'
 
 function TodoListWidget({ title, count }){
-	return (
-		<div class="container">
-			<div class="list-item">
-				<span class="list-title">{ title }</span>
-				<span class="list-count">({ count })</span>
-			</div>
-			<img class="icon-trash" src={ trash }/>
-		</div>
-	)
+  return (
+    <div class="container">
+      <div class="list-item">
+        <span class="list-title">{ title }</span>
+        <span class="list-count">({ count })</span>
+      </div>
+      <img class="icon-trash" src={ trash }/>
+    </div>
+  )
 }
 
 export default TodoListWidget;
