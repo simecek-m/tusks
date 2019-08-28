e3e3aeb9824f5e9a6e227567728e9c66bf1c75cb
Martin Šimeček <simecek.m@seznam.cz>
2019-08-28 17:23:32 +0200

    create protected route component

---
+++ src/router/ProtectedRouter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+function ProtectedRoute ({ component, user, ...rest }) {
+  return (
+    user ? <Route { ...rest } component = { component } /> : <Redirect to="/login" />
+  )
+};
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(ProtectedRoute);
