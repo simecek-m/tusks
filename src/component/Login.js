3f93cae5e8e2f589119b2d856cac9c70a24b8d63
Martin Šimeček <simecek.m@seznam.cz>
2019-08-28 10:46:25 +0200

    refactor login methods HTML template

--- src/component/Login.js
+++ src/component/Login.js
@@ -15,19 +15,19 @@ function Login() {
     <div>
       <h1 className="title">React-To-Do</h1>
       <h2 className="sub-title">Login into our todo app through your Google account.</h2>
-      <GoogleLogin
-        clientId={process.env.REACT_APP_GOOGLE_API_CLIENT_ID}
-        onSuccess={loginSuccessCallback}
-        onFailure={loginFailCallback}
-        render={renderProps => (
-          <div className="login-methods">
+      <div className="login-methods">
+        <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_API_CLIENT_ID}
+          onSuccess={loginSuccessCallback}
+          onFailure={loginFailCallback}
+          render={renderProps => (
             <div onClick={renderProps.onClick} className="login-method">
               <FontAwesomeIcon className="icon" icon={ faGoogle }/>
               <span className="authority">Google</span>
             </div>
-          </div>
-        )}
-      />
+          )}
+        />
+      </div>
     </div>
   )
 }
