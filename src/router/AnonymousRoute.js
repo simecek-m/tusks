50db2f21c8f385c65b43cec762897addfbb263d5
Martin Šimeček <simecek.m@seznam.cz>
2019-08-28 20:05:26 +0200

    create AnonymousRoute component

---
+++ src/router/AnonymousRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+function AnonymousRoute ({ component, user, ...rest }) {
+  return (
+    user ? <Redirect to="/" /> : <Route { ...rest } component = { component } />
+  )
+};
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(AnonymousRoute);
