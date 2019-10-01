4e0f78dcefa96b192132c47c266cd2e9e31f1aac
Martin Šimeček <simecek.m@seznam.cz>
2019-10-01 22:21:31 +0200

    integrate Loading component into Todo Lists page

--- src/component/Todos.js
+++ src/component/Todos.js
@@ -3,9 +3,16 @@ import TodoListWidget from "component/TodoListWidget";
 import UserWidget from "component/UserWidget";
 import { connect } from "react-redux";
 import api, { setAuthorizationHeader } from "api";
+import Loading from "component/Loading";
+import Lottie from "react-lottie";
+import writeAnimation from "assets/animation/write.json";
+import Button from "component/Button";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 class Todos extends React.Component {
   state = {
+    loading: true,
+    error: false,
     list: []
   };
 
@@ -14,10 +21,16 @@ class Todos extends React.Component {
       .get("/todos", setAuthorizationHeader(this.props.user))
       .then(response =>
         this.setState({
-          list: response.data
+          list: response.data,
+          loading: false
         })
       )
-      .catch(error => console.error(error));
+      .catch(() => {
+        this.setState({
+          error: true,
+          loading: false
+        });
+      });
   }
 
   render() {
@@ -28,12 +41,35 @@ class Todos extends React.Component {
         count={item.tasks.length}
       />
     ));
-
+    const writeAnimationOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: writeAnimation,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+      }
+    };
+    const writeFirstTodoAnimation = (
+      <div>
+        <h2>Write your first todo list:</h2>
+        <Lottie
+          options={writeAnimationOptions}
+          width={400}
+          height={300}
+          style={{ "margin-bottom": "50px" }}
+          isClickToPauseDisabled={true}
+        />
+        <Button icon={faPlus} text="Create" />
+      </div>
+    );
+    const content = items.length > 0 ? items : writeFirstTodoAnimation;
     return (
       <div>
         <UserWidget />
         <h1 className="title">React-To-Do</h1>
-        {items}
+        <Loading loading={this.state.loading} error={this.state.error}>
+          {content}
+        </Loading>
       </div>
     );
   }
