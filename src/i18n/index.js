46fd0cd10115f0a623890a3c3373a114fdcefaad
Martin Šimeček <simecek.m@seznam.cz>
2019-10-17 23:41:54 +0200

    sync used locale by i18next and stored one in redux

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,14 +1,22 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import store from "store/index";
 
 import cs from "i18n/locale/cs.json";
 import en from "i18n/locale/en.json";
 
-const DEFAULT_LANGUAGE = "en";
+export const DEFAULT_LOCALE = process.env.REACT_APP_DEFAULT_LOCALE;
+
+store.subscribe(() => {
+  const storedLocale = store.getState().locale;
+  if (storedLocale !== i18n.language) {
+    i18n.changeLanguage(storedLocale);
+  }
+});
 
 i18n.use(initReactI18next).init({
-  fallbackLng: DEFAULT_LANGUAGE,
-  lng: "cs",
+  fallbackLng: DEFAULT_LOCALE,
+  lng: DEFAULT_LOCALE,
   debug: process.env.NODE_ENV === "development",
   interpolation: {
     escapeValue: false
