1f5530cd6250035e2e02d8a720dcc7c143c6fc66
Martin Šimeček <simecek.m@seznam.cz>
2019-10-25 16:04:36 +0200

    add constructor for unsupported feature warning notification

--- src/notification/index.js
+++ src/notification/index.js
@@ -1,10 +1,28 @@
 import { store } from "react-notifications-component";
+import i18n from "i18n";
 
 export function showDangerNotificationWithStatus(title = "Error", status = 0) {
   store.addNotification({
     title: title.toUpperCase(),
     message: `Status: ${status}`,
     type: "danger",
+    container: "bottom-right",
+    animationIn: ["animated", "bounceIn"],
+    animationOut: ["animated", "bounceOut"],
+    dismiss: {
+      duration: 5000,
+      click: true,
+      pauseOnHover: true,
+      waitForAnimation: true
+    }
+  });
+}
+
+export function showUnsupportedFeatureNotification() {
+  store.addNotification({
+    title: i18n.t("notification.unsupportedFeature"),
+    message: i18n.t("notification.unsupportedFeatureDescription"),
+    type: "warning",
     container: "bottom-right",
     animationIn: ["animated", "bounceIn"],
     animationOut: ["animated", "bounceOut"],
