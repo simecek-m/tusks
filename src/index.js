e26fff4d10a019f741e9cdc048fff65950c92c1e
Martin Šimeček <simecek.m@seznam.cz>
2019-10-25 11:39:24 +0200

    use notification component

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "i18n";
+import ReactNotification from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
 
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <I18nextProvider i18n={i18n}>
+        <ReactNotification />
         <App />
       </I18nextProvider>
     </PersistGate>
