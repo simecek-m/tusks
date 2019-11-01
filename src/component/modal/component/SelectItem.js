20b549e8b60de8a75483d81b7ee94aef16224f2f
Martin Šimeček <simecek.m@seznam.cz>
2019-11-01 12:17:49 +0100

    add arrow for currently selected option in SelectItem

--- src/component/modal/component/SelectItem.js
+++ src/component/modal/component/SelectItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "component/modal/component/SelectItem.sass";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function SelectItem({
   image = null,
@@ -11,6 +13,9 @@ function SelectItem({
   ) : null;
   return (
     <div className="select-item-component" onClick={onClick}>
+      <span className="arrow">
+        <FontAwesomeIcon icon={faArrowRight} />
+      </span>
       {img}
       <span className="text">{text}</span>
     </div>
