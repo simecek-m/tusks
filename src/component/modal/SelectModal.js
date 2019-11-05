c22289decb25faf4491e8f59d781080c692f5976
Martin Šimeček <simecek.m@seznam.cz>
2019-11-05 09:35:00 +0100

    SelectModal component use Context API

--- src/component/modal/SelectModal.js
+++ src/component/modal/SelectModal.js
@@ -1,22 +1,33 @@
 import React from "react";
+import { withModal } from "modal/withModal";
+import SelectItem from "component/modal/component/SelectItem";
 import "component/modal/SelectModal.sass";
 
-function SelectModal({
-  title = "Title",
-  text = "Modal description text",
-  visible = false,
-  onClose = () => {},
-  children
-}) {
-  return visible ? (
-    <div className="select-modal-component" onClick={() => onClose()}>
+function SelectModal({ modal, closeModal }) {
+  const options = modal.options
+    ? modal.options.map((option, index) => (
+        <SelectItem
+          key={index}
+          image={option.image}
+          text={option.text}
+          onClick={() => {
+            if (option.onClick) {
+              option.onClick();
+              closeModal();
+            }
+          }}
+        />
+      ))
+    : null;
+  return modal.visible ? (
+    <div className="select-modal-component" onClick={() => closeModal()}>
       <div className="modal-body" onClick={e => e.stopPropagation()}>
-        <h2 className="modal-title">{title}</h2>
-        <div className="modal-description">{text}</div>
-        <div className="modal-selection">{children}</div>
+        <h2 className="modal-title">{modal.title}</h2>
+        <div className="modal-description">{modal.text}</div>
+        {options}
       </div>
     </div>
   ) : null;
 }
 
-export default SelectModal;
+export default withModal(SelectModal);
