69e1c240874a3cb9223c2df92a79e1f55e92b0fe
Martin Šimeček <simecek.m@seznam.cz>
2020-02-07 10:32:30 +0100

    use Enzyme shallow rendering for Button component

--- src/component/button/Button.test.js
+++ src/component/button/Button.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "component/button/Button";
-import renderer from "react-test-renderer";
 import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { shallow } from "enzyme";
 
 const TEST_BUTTON_CUSTOM_TYPE = "warn";
 const TEST_BUTTON_CUSTOM_TEXT = "send";
@@ -10,21 +10,19 @@ const TEST_BUTTON_CUSTOM_CALLBACK = () => {};
 
 describe("Button component", () => {
   test("should render default Button", () => {
-    const component = renderer.create(<Button />).toJSON();
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(<Button />);
+    expect(wrapper).toMatchSnapshot();
   });
 
   test("should render custom Button", () => {
-    const component = renderer
-      .create(
-        <Button
-          icon={TEST_BUTTON_CUSTOM_ICON}
-          text={TEST_BUTTON_CUSTOM_TEXT}
-          onClick={TEST_BUTTON_CUSTOM_CALLBACK}
-          type={TEST_BUTTON_CUSTOM_TYPE}
-        />
-      )
-      .toJSON();
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(
+      <Button
+        icon={TEST_BUTTON_CUSTOM_ICON}
+        text={TEST_BUTTON_CUSTOM_TEXT}
+        onClick={TEST_BUTTON_CUSTOM_CALLBACK}
+        type={TEST_BUTTON_CUSTOM_TYPE}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 });
