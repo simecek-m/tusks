054b93dabdbd2622d2b7d1cbc3f8d7fffc96ba48
Martin Šimeček <simecek.m@seznam.cz>
2020-02-07 10:47:23 +0100

    use Enzyme shallow rendering for Loading component

--- src/component/animation/Loading.test.js
+++ src/component/animation/Loading.test.js
@@ -1,28 +1,26 @@
 import React from "react";
 import Loading from "component/animation/Loading";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
 const TEST_LOADING_COMPLETE_HTML = <div>LOADING COMPLETED</div>;
 
 describe("Loading component", () => {
   test("should render completed Loading", () => {
-    const component = renderer
-      .create(<Loading>{TEST_LOADING_COMPLETE_HTML}</Loading>)
-      .toJSON();
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(<Loading>{TEST_LOADING_COMPLETE_HTML}</Loading>);
+    expect(wrapper).toMatchSnapshot();
   });
 
   test("should render Loading in progress", () => {
-    const component = renderer
-      .create(<Loading loading={true}>{TEST_LOADING_COMPLETE_HTML}</Loading>)
-      .toJSON();
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(
+      <Loading loading={true}>{TEST_LOADING_COMPLETE_HTML}</Loading>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 
   test("should render Loading with error", () => {
-    const component = renderer
-      .create(<Loading error={true}>{TEST_LOADING_COMPLETE_HTML}</Loading>)
-      .toJSON();
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(
+      <Loading error={true}>{TEST_LOADING_COMPLETE_HTML}</Loading>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 });
