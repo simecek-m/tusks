f8f0d02d839621783cefa48965ad3d9b8144523e
Martin Šimeček <simecek.m@seznam.cz>
2020-02-07 10:57:35 +0100

    use Enzyme shallow rendering for Title component

--- src/component/common/Title.test.js
+++ src/component/common/Title.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Title from "component/common/Title";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 import { faDatabase } from "@fortawesome/free-solid-svg-icons";
 
 const TEST_CUSTOM_TITLE_TEXT = "Profile";
@@ -8,23 +8,21 @@ const TEST_CUSTOM_TITLE_ICON = faDatabase;
 
 describe("Title component", () => {
   test("should render default Title", () => {
-    const component = renderer.create(<Title />).toJSON();
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(<Title />);
+    expect(wrapper).toMatchSnapshot();
   });
 
   test("should render custom Title", () => {
-    const component = renderer
-      .create(
-        <Title text={TEST_CUSTOM_TITLE_TEXT} icon={TEST_CUSTOM_TITLE_ICON} />
-      )
-      .toJSON();
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(
+      <Title text={TEST_CUSTOM_TITLE_TEXT} icon={TEST_CUSTOM_TITLE_ICON} />
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 
   test("should render custom Title without icon", () => {
-    const component = renderer
-      .create(<Title text={TEST_CUSTOM_TITLE_TEXT} icon={null} />)
-      .toJSON();
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(
+      <Title text={TEST_CUSTOM_TITLE_TEXT} icon={null} />
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 });
