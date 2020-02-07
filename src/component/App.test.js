61d6d9cfe2da554e9b4fe0763d03a61c0706823f
Martin Šimeček <simecek.m@seznam.cz>
2020-02-07 10:31:12 +0100

    use Enzyme shallow rendering for App component

--- src/component/App.test.js
+++ src/component/App.test.js
@@ -1,28 +1,10 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import App from "component/App";
-import { Provider } from "react-redux";
-import { createPersistor, getStore } from "store";
-
-jest.mock("react-i18next", () => ({
-  withTranslation: () => Component => {
-    Component.defaultProps = { ...Component.defaultProps, t: key => key };
-    return Component;
-  }
-}));
-
-createPersistor();
-const store = getStore();
+import { shallow } from "enzyme";
 
 describe("App component", () => {
   test("should render App", () => {
-    const component = renderer
-      .create(
-        <Provider store={store}>
-          <App />
-        </Provider>
-      )
-      .toJSON();
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
   });
 });
