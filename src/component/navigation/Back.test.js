bab9ba82a20601a02842a4bae7a2ed1f2e085fec
Martin Šimeček <simecek.m@seznam.cz>
2020-02-10 16:50:54 +0100

    add test for goBack method in Back component

--- src/component/navigation/Back.test.js
+++ src/component/navigation/Back.test.js
@@ -2,9 +2,21 @@ import React from "react";
 import { Back } from "component/navigation/Back";
 import { shallow } from "enzyme";
 
+const mockHistory = {
+  goBack: () => {}
+};
+
+const spy = jest.spyOn(mockHistory, "goBack");
+
 describe("Back component", () => {
   test("should render back", () => {
     const wrapper = shallow(<Back />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should return to previous page", () => {
+    const wrapper = shallow(<Back history={mockHistory} />);
+    wrapper.find("#back-component").simulate("click");
+    expect(spy).toBeCalledTimes(1);
   });
 });
