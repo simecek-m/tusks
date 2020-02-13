07e36302a38d4312c3ca9642466efb29c07f4a88
Martin Šimeček <simecek.m@seznam.cz>
2020-02-13 10:10:23 +0100

    test mocked user JWT in TodoList component api call

--- src/component/page/TodoList.test.js
+++ src/component/page/TodoList.test.js
@@ -15,6 +15,8 @@ const INIT_STATE = {
   todoList: null
 };
 
+const TEST_USER_JWT = "xxxxx.yyyyy.zzzzz";
+
 const TEST_TODO_LIST_ORIGINAL = {
   title: "Work",
   tasks: [
@@ -59,13 +61,16 @@ describe("TodoList component", () => {
   test("should fetch data from API after mounting", async () => {
     const TEST_TODO_LIST = JSON.parse(JSON.stringify(TEST_TODO_LIST_ORIGINAL));
     apiGetSpy.mockResolvedValue({ data: TEST_TODO_LIST });
-    const wrapper = shallow(<TodoList match={MOCK_ROUTER} />, {
-      disableLifecycleMethods: true
-    });
+    const wrapper = shallow(
+      <TodoList match={MOCK_ROUTER} user={TEST_USER_JWT} />,
+      {
+        disableLifecycleMethods: true
+      }
+    );
     await wrapper.instance().componentDidMount();
     expect(apiGetSpy).toHaveBeenCalledTimes(1);
     expect(apiGetSpy).toHaveBeenCalledWith(`/todos/${MOCK_ROUTER.params.id}`, {
-      headers: { Authorization: "Bearer undefined" }
+      headers: { Authorization: `Bearer ${TEST_USER_JWT}` }
     });
     const state = wrapper.state();
     expect(state.todoList).toEqual(TEST_TODO_LIST_ORIGINAL);
@@ -76,13 +81,16 @@ describe("TodoList component", () => {
 
   test("should change state after error", async () => {
     apiGetSpy.mockRejectedValue({ status: 401 });
-    const wrapper = shallow(<TodoList match={MOCK_ROUTER} />, {
-      disableLifecycleMethods: true
-    });
+    const wrapper = shallow(
+      <TodoList match={MOCK_ROUTER} user={TEST_USER_JWT} />,
+      {
+        disableLifecycleMethods: true
+      }
+    );
     await wrapper.instance().componentDidMount();
     expect(apiGetSpy).toHaveBeenCalledTimes(1);
     expect(apiGetSpy).toHaveBeenCalledWith(`/todos/${MOCK_ROUTER.params.id}`, {
-      headers: { Authorization: "Bearer undefined" }
+      headers: { Authorization: `Bearer ${TEST_USER_JWT}` }
     });
     await wrapper.update();
     const state = wrapper.state();
@@ -95,7 +103,9 @@ describe("TodoList component", () => {
     const NEW_TASK = { _id: 1, text: "buy milk", completed: false };
     apiPostSpy.mockResolvedValue({ data: NEW_TASK });
     apiGetSpy.mockResolvedValue({ data: { title: "Shop", tasks: [] } });
-    const wrapper = await shallow(<TodoList match={MOCK_ROUTER} />);
+    const wrapper = await shallow(
+      <TodoList match={MOCK_ROUTER} user={TEST_USER_JWT} />
+    );
     let state = wrapper.state();
     expect(apiGetSpy).toHaveBeenCalledTimes(1);
     expect(state.todoList.tasks).toEqual([]);
@@ -104,7 +114,7 @@ describe("TodoList component", () => {
     expect(apiPostSpy).toHaveBeenCalledWith(
       `/todos/${MOCK_ROUTER.params.id}/tasks`,
       { text: NEW_TASK.text, completed: false },
-      { headers: { Authorization: "Bearer undefined" } }
+      { headers: { Authorization: `Bearer ${TEST_USER_JWT}` } }
     );
     state = wrapper.state();
     expect(state.todoList.tasks).toEqual([NEW_TASK]);
@@ -121,7 +131,9 @@ describe("TodoList component", () => {
       .tasks[UPDATED_TASK_INDEX].completed;
     apiGetSpy.mockResolvedValue({ data: TEST_TODO_LIST });
     apiPutSpy.mockResolvedValue({});
-    const wrapper = await shallow(<TodoList match={MOCK_ROUTER} />);
+    const wrapper = await shallow(
+      <TodoList match={MOCK_ROUTER} user={TEST_USER_JWT} />
+    );
     const state = wrapper.state();
     expect(state.todoList.tasks).toEqual(TEST_TODO_LIST_ORIGINAL.tasks);
     await wrapper.instance().updateTask(UPDATED_TASK_INDEX);
@@ -129,7 +141,7 @@ describe("TodoList component", () => {
     expect(apiPutSpy).toHaveBeenCalledWith(
       `/todos/${MOCK_ROUTER.params.id}/tasks/${TEST_TODO_LIST.tasks[UPDATED_TASK_INDEX]._id}`,
       UPDATED_TODO_LIST.tasks[UPDATED_TASK_INDEX],
-      { headers: { Authorization: "Bearer undefined" } }
+      { headers: { Authorization: `Bearer ${TEST_USER_JWT}` } }
     );
     expect(state.todoList.tasks).toEqual(UPDATED_TODO_LIST.tasks);
     expect(wrapper).toMatchSnapshot();
@@ -142,7 +154,9 @@ describe("TodoList component", () => {
     apiGetSpy.mockResolvedValue({
       data: JSON.parse(JSON.stringify(TODO_LIST))
     });
-    const wrapper = await shallow(<TodoList match={MOCK_ROUTER} />);
+    const wrapper = await shallow(
+      <TodoList match={MOCK_ROUTER} user={TEST_USER_JWT} />
+    );
     const state = wrapper.state();
     expect(state.error).toBeFalsy();
     expect(state.loading).toBeFalsy();
@@ -151,7 +165,7 @@ describe("TodoList component", () => {
     expect(apiDeleteSpy).toHaveBeenCalledTimes(1);
     expect(apiDeleteSpy).toHaveBeenCalledWith(
       `/todos/${MOCK_ROUTER.params.id}/tasks/${TASK._id}`,
-      { headers: { Authorization: "Bearer undefined" } }
+      { headers: { Authorization: `Bearer ${TEST_USER_JWT}` } }
     );
     expect(state.todoList.tasks).toEqual([]);
     expect(wrapper).toMatchSnapshot();
