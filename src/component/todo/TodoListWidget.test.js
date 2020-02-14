de76457a4db7fee684cb3143277b619cd187a9b4
Martin Šimeček <simecek.m@seznam.cz>
2020-02-14 10:51:59 +0100

    add logic test for TodoListWidget component

--- src/component/todo/TodoListWidget.test.js
+++ src/component/todo/TodoListWidget.test.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { TodoListWidget } from "component/todo/TodoListWidget";
 import { shallow } from "enzyme";
+import { CONFIRMATION } from "modal/types";
+
+jest.mock("react-i18next", () => ({
+  withTranslation: () => Component => {
+    Component.defaultProps = { ...Component.defaultProps, t: key => key };
+    return Component;
+  }
+}));
 
 describe("TodoListWidget component", () => {
   test("should render default", () => {
@@ -11,5 +19,20 @@ describe("TodoListWidget component", () => {
   test("should render Work list", () => {
     const wrapper = shallow(<TodoListWidget title="Work" count={6} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should open modal", async () => {
+    const mockOpenModal = jest.fn();
+    const wrapper = await shallow(<TodoListWidget openModal={mockOpenModal} />);
+    const elem = wrapper.find(".icon-trash");
+    await elem.simulate("click", { stopPropagation: () => {} });
+    expect(mockOpenModal).toHaveBeenCalledTimes(1);
+    expect(mockOpenModal).toHaveBeenCalledWith({
+      text: "todoListWidget.modal.text",
+      title: "todoListWidget.modal.title",
+      type: CONFIRMATION,
+      onConfirm: expect.any(Function)
+    });
+    mockOpenModal.mockRestore();
   });
 });
