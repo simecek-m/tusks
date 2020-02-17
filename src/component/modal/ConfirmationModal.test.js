13256b8a09f1c51f2514c39acf7c609f0b941f78
Martin Šimeček <simecek.m@seznam.cz>
2020-02-17 12:52:10 +0100

    add test for ConfirmationModal logic

--- src/component/modal/ConfirmationModal.test.js
+++ src/component/modal/ConfirmationModal.test.js
@@ -10,12 +10,14 @@ jest.mock("react-i18next", () => ({
   }
 }));
 
+const confirmMock = jest.fn();
+
 const TEST_MODAL_CONFIRMATION = {
   type: CONFIRMATION,
   visible: true,
   title: "Delete",
   text: "This action will permanently delete 'TEST' todo list! Are you sure?",
-  onConfirm: () => {}
+  onConfirm: confirmMock
 };
 
 describe("Confirmation modal", () => {
@@ -29,5 +31,49 @@ describe("Confirmation modal", () => {
       <ConfirmationModal modal={TEST_MODAL_CONFIRMATION} />
     );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should close modal", () => {
+    const closeModalMock = jest.fn();
+    const wrapper = shallow(
+      <ConfirmationModal
+        modal={TEST_MODAL_CONFIRMATION}
+        closeModal={closeModalMock}
+      />
+    );
+    const elem = wrapper.find(".confirmation-modal-component");
+    expect(elem).not.toBeNull();
+    elem.simulate("click");
+    expect(closeModalMock).toHaveBeenCalledTimes(1);
+    closeModalMock.mockRestore();
+  });
+
+  test("should stop propagate close modal click event on body element", () => {
+    const stopPropagationMock = jest.fn();
+    const wrapper = shallow(
+      <ConfirmationModal modal={TEST_MODAL_CONFIRMATION} />
+    );
+    const elem = wrapper.find(".modal-body");
+    expect(elem).not.toBeNull();
+    elem.simulate("click", { stopPropagation: stopPropagationMock });
+    expect(stopPropagationMock).toHaveBeenCalledTimes(1);
+    stopPropagationMock.mockRestore();
+  });
+
+  test("should close modal after confirmation", () => {
+    const closeModalMock = jest.fn();
+    const wrapper = shallow(
+      <ConfirmationModal
+        modal={TEST_MODAL_CONFIRMATION}
+        closeModal={closeModalMock}
+      />
+    );
+    const elem = wrapper.find(".confirmation-text");
+    expect(elem).not.toBeNull();
+    elem.simulate("click");
+    expect(confirmMock).toHaveBeenCalledTimes(1);
+    expect(closeModalMock).toHaveBeenCalledTimes(1);
+    confirmMock.mockReset();
+    closeModalMock.mockReset();
   });
 });
