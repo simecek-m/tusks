e737a81dab111963e234bae53c3c195839868b0f
Martin Šimeček <simecek.m@seznam.cz>
2020-02-20 10:39:17 +0100

    add test for InputModal component

--- src/component/modal/InputModal.test.js
+++ src/component/modal/InputModal.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { InputModal } from "component/modal/InputModal";
 import { INPUT } from "modal/types";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 jest.mock("react-i18next", () => ({
   withTranslation: () => Component => {
@@ -10,11 +10,14 @@ jest.mock("react-i18next", () => ({
   }
 }));
 
+const confirmMock = jest.fn();
+
 const TEST_MODAL_INPUT = {
   type: INPUT,
   visible: true,
   title: "New list",
-  text: "Choose name for your new todo list:"
+  text: "Choose name for your new todo list:",
+  onConfirm: confirmMock
 };
 
 describe("Input modal", () => {
@@ -26,5 +29,58 @@ describe("Input modal", () => {
   test("should render modal", () => {
     const wrapper = shallow(<InputModal modal={TEST_MODAL_INPUT} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should close modal", () => {
+    const closeModalMock = jest.fn();
+    const wrapper = shallow(
+      <InputModal modal={TEST_MODAL_INPUT} closeModal={closeModalMock} />
+    );
+    const elem = wrapper.find(".input-modal-component");
+    expect(elem).not.toBeNull();
+    elem.simulate("click");
+    expect(closeModalMock).toHaveBeenCalledTimes(1);
+    closeModalMock.mockRestore();
+  });
+
+  test("should stop propagate close modal click event on body element", () => {
+    const stopPropagationMock = jest.fn();
+    const wrapper = shallow(<InputModal modal={TEST_MODAL_INPUT} />);
+    const elem = wrapper.find(".modal-body");
+    expect(elem).not.toBeNull();
+    elem.simulate("click", { stopPropagation: stopPropagationMock });
+    expect(stopPropagationMock).toHaveBeenCalledTimes(1);
+    stopPropagationMock.mockRestore();
+  });
+
+  test("should change state after change input field", () => {
+    const FIRST_VALUE = "first";
+    const SECOND_VALUE = "second";
+    const wrapper = mount(<InputModal modal={TEST_MODAL_INPUT} />);
+    const inputElem = wrapper.find(".modal-input");
+    inputElem.simulate("change", { target: { value: FIRST_VALUE } });
+    let state = wrapper.state();
+    expect(state.value).toEqual(FIRST_VALUE);
+    inputElem.simulate("change", { target: { value: SECOND_VALUE } });
+    state = wrapper.state();
+    expect(state.value).toEqual(SECOND_VALUE);
+  });
+
+  test("should close modal after confirmation", () => {
+    const TEST_VALUE = "whatever";
+    const closeModalMock = jest.fn();
+    const wrapper = mount(
+      <InputModal modal={TEST_MODAL_INPUT} closeModal={closeModalMock} />
+    );
+    const inputElem = wrapper.find(".modal-input");
+    inputElem.simulate("change", { target: { value: "whatever" } });
+    const elem = wrapper.find(".modal-confirmation-button");
+    expect(elem).not.toBeNull();
+    elem.simulate("submit");
+    expect(confirmMock).toHaveBeenCalledTimes(1);
+    expect(confirmMock).toHaveBeenCalledWith(TEST_VALUE);
+    expect(closeModalMock).toHaveBeenCalledTimes(1);
+    confirmMock.mockReset();
+    closeModalMock.mockReset();
   });
 });
