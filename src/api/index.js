5bd582163109d608052a75015c76d5b87296663c
Martin Šimeček <simecek.m@seznam.cz>
2020-02-27 10:30:59 +0100

    test API interceptors handlers

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const DEFAULT_FALLBACK_RESPONSE = {
+export const DEFAULT_FALLBACK_RESPONSE = {
   data: {
     message: "Could not get any response!"
   },
   status: 500
 };
 
-const config = {
+export const config = {
   baseURL: `${process.env.REACT_APP_TODO_BACKEND_HOST}:${process.env.REACT_APP_TODO_BACKEND_PORT}/api`
 };
 
@@ -18,12 +18,16 @@ export function setAuthorizationHeader(token) {
 const api = axios.create(config);
 api.interceptors.response.use(
   function(response) {
+    console.log(response);
     return response;
   },
   function(error) {
+    console.log("error: ", error);
+    console.log("error response: ", error.response);
     if (error.response === undefined) {
       error.response = DEFAULT_FALLBACK_RESPONSE;
     }
+    console.log("finished error: ", error);
     return Promise.reject(error);
   }
 );
--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,7 +1,23 @@
 import api, { setAuthorizationHeader } from "api";
+import { config, DEFAULT_FALLBACK_RESPONSE } from "api";
 
 const DEFAULT_URL = `${process.env.REACT_APP_TODO_BACKEND_HOST}:${process.env.REACT_APP_TODO_BACKEND_PORT}/api`;
 const TEST_TOKEN = "TEST_TOKEN";
+
+const TEST_FULFILLED_RESPONSE = {
+  status: 200,
+  data: {
+    message: "RESPONSE FULFILLED"
+  }
+};
+
+const TEST_REJECTED_RESPONSE = {
+  response: {
+    data: {
+      message: "RESPONSE REJECTED"
+    }
+  }
+};
 
 describe("API", () => {
   test("should create axios instance with API URL", () => {
@@ -17,6 +33,35 @@ describe("API", () => {
     const header = setAuthorizationHeader(TEST_TOKEN);
     expect(header).toEqual({
       headers: { Authorization: `Bearer ${TEST_TOKEN}` }
+    });
+  });
+
+  test("should return config option", () => {
+    expect(config.baseURL).toEqual(DEFAULT_URL);
+  });
+
+  test("should pass fullfiled response", () => {
+    const result = api.interceptors.response.handlers[0].fulfilled(
+      TEST_FULFILLED_RESPONSE
+    );
+    expect(result).toEqual(TEST_FULFILLED_RESPONSE);
+  });
+
+  test("should return rejected response", done => {
+    const result = api.interceptors.response.handlers[0].rejected(
+      TEST_REJECTED_RESPONSE
+    );
+    result.catch(error => {
+      expect(error).toEqual(TEST_REJECTED_RESPONSE);
+      done();
+    });
+  });
+
+  test("should return default rejected response", done => {
+    const result = api.interceptors.response.handlers[0].rejected({});
+    result.catch(error => {
+      expect(error.response).toEqual(DEFAULT_FALLBACK_RESPONSE);
+      done();
     });
   });
 });
