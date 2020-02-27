dde1eb42ff26832edc73ebe75f5175e5d6f81c2d
Martin Šimeček <simecek.m@seznam.cz>
2020-02-27 11:49:52 +0100

    add test for router script

---
+++ src/router/index.test.js
@@ -0,0 +1,16 @@
+import { history } from "router";
+
+describe("router", () => {
+  test("should return history object", () => {
+    expect(history).not.toBeNull();
+    expect(history.length).toEqual(1);
+    expect(history).toEqual(
+      expect.objectContaining({
+        location: expect.any(Object),
+        push: expect.any(Function),
+        go: expect.any(Function),
+        goBack: expect.any(Function)
+      })
+    );
+  });
+});
