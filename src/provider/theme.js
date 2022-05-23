78ef3daf837bf7c7db870f9eb72046673884fa5e
Martin Šimeček <simecek.m@seznam.cz>
2022-05-23 19:08:22 +0200

    implement persist theme on reload feature

--- src/provider/theme.js
+++ src/provider/theme.js
@@ -4,22 +4,34 @@ import { DEFAULT_THEME } from "conf";
 const ThemeContext = createContext(DEFAULT_THEME);
 export const useTheme = () => useContext(ThemeContext);
 
+const THEME_KEY = "theme";
+
 export default function ThemeProvider({
   defaulValue = DEFAULT_THEME,
   children,
 }) {
-  const [theme, setTheme] = useState(defaulValue);
-  const switchTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
+  const persistTheme = (theme) => {
+    localStorage.setItem(THEME_KEY, theme);
   };
+
+  const getPersistedTheme = () => {
+    return localStorage.getItem(THEME_KEY);
+  };
+
+  const persistedTheme = getPersistedTheme();
+  const [theme, setTheme] = useState(persistedTheme ?? defaulValue);
+
+  const switchTheme = () => {
+    const switchedTheme = theme === "light" ? "dark" : "light";
+    setTheme(switchedTheme);
+    persistTheme(switchedTheme);
+  };
+
   const contextValue = {
     theme,
     switchTheme,
   };
+
   return (
     <ThemeContext.Provider value={contextValue}>
       {children}
