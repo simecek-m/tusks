0d9f21ed71eb5584ff6cc7a8909418f21708b5bb
Martin Šimeček <simecek.m@seznam.cz>
2022-05-25 21:17:58 +0200

    remove outdated tests

--- src/setupTests.js
+++
@@ -1,6 +0,0 @@
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import { createSerializer } from "enzyme-to-json";
-
-configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(createSerializer({ ignoreDefaultProps: true }));
