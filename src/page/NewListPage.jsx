22d44286d007a7cf6faeccb15b7469790064bc49
Martin Šimeček <simecek.m@seznam.cz>
2022-06-02 20:29:05 +0200

    add list of all icons

--- src/page/NewListPage.jsx
+++ src/page/NewListPage.jsx
@@ -1,3 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AVAILABLE_ICONS from "constants/icons";
+import "page/NewListPage.sass";
+
 export default function NewListPage() {
-  return <h1>New List Page</h1>;
+  return (
+    <div className="page-layout-new-list">
+      <h1>Icon</h1>
+      <div className="icon-picker">
+        {AVAILABLE_ICONS.map((icon, index) => (
+          <FontAwesomeIcon icon={icon} className="icon" key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
