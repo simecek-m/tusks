f9189a0eb64995ffacb09456fe4a9307d0e1693b
Martin Šimeček <simecek.m@seznam.cz>
2022-08-22 19:49:21 +0200

    add illustration to Not Found page

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,6 +1,7 @@
 import Button from "component/button/Button";
 import styles from "page/NotFound.module.sass";
 import { useNavigate } from "react-router-dom";
+import { ReactComponent as NotFundIllustration } from "assets/illustration/not-found.svg";
 
 export default function NotFound() {
   const navigate = useNavigate();
@@ -9,11 +10,21 @@ export default function NotFound() {
   };
   return (
     <div id={styles["not-found-page"]}>
-      <div>Not Found</div>
-      <div>404</div>
-      <Button icon="home" onClick={navigateHome}>
-        home
-      </Button>
+      <div className={styles["horizontal-stack"]}>
+        <div>
+          <h1>Not Found</h1>
+          <p>404</p>
+          <Button icon="home" onClick={navigateHome}>
+            home
+          </Button>
+        </div>
+        <div>
+          <NotFundIllustration
+            id={styles["not-found-illustration"]}
+            height="40vh"
+          />
+        </div>
+      </div>
     </div>
   );
 }
