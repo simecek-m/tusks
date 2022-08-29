c4d829b7cc4851a9ff33f51d7d2449998e820b6c
Martin Šimeček <simecek.m@seznam.cz>
2022-08-29 19:38:28 +0200

    add onDelete prop to Task component

--- src/component/task/Task.jsx
+++ src/component/task/Task.jsx
@@ -1,19 +1,27 @@
 import { bool, string } from "prop-types";
 import { useState } from "react";
 import styles from "component/task/Task.module.sass";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Task({ isComplete, id, text }) {
-  const [checked, setChecked] = useState(isComplete);
+export default function Task({ completed, id, text, onDelete }) {
+  const [checked, setChecked] = useState(completed);
   return (
     <span className={styles.checkbox}>
-      <input
-        className="taskcheckbox"
-        type="checkbox"
-        id={id}
-        checked={checked}
-        onChange={() => setChecked(!checked)}
+      <label htmlFor={id}>
+        <input
+          className="taskcheckbox"
+          type="checkbox"
+          id={id}
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+        />
+        <span>{text}</span>
+      </label>
+      <FontAwesomeIcon
+        icon="close"
+        className={styles["close-icon"]}
+        onClick={() => onDelete(id)}
       />
-      <label htmlFor={id}>{text}</label>
     </span>
   );
 }
