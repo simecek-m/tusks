221196f948ad6bc404652234d914c61f5ebc6716
Martin Šimeček <simecek.m@seznam.cz>
2023-01-21 16:41:31 +0100

    remove all components

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import App from "component/App";
 import * as serviceWorker from "serviceWorker";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import ThemeProvider from "provider/theme";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
@@ -19,18 +18,16 @@ const queryClient = new QueryClient({
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeProvider>
-    <QueryClientProvider client={queryClient}>
-      <Auth0Provider
-        audience="todo-server"
-        domain={process.env.REACT_APP_AUTH_DOMAIN}
-        clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-        redirectUri={window.location.origin}
-      >
-        <App />
-      </Auth0Provider>
-    </QueryClientProvider>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <Auth0Provider
+      audience="todo-server"
+      domain={process.env.REACT_APP_AUTH_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </QueryClientProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
