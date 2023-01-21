8b19412eee93778f28ec3fd2b43b77dfea4682ed
Martin Šimeček <simecek.m@seznam.cz>
2023-01-21 18:03:09 +0100

    update dependencies and structure of project

--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,9 @@
-import React from "react";
+import App from "App";
 import ReactDOM from "react-dom/client";
-import App from "component/App";
 import * as serviceWorker from "serviceWorker";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <Auth0Provider
-      audience="todo-server"
-      domain={process.env.REACT_APP_AUTH_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-      redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0Provider>
-  </QueryClientProvider>
-);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
