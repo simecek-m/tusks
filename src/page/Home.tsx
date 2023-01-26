4438484a11fbf48d0d40cd5a9c40f4b730344651
Martin Šimeček <simecek.m@seznam.cz>
2023-01-26 22:16:44 +0100

    add basic home page without login button

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <h1 className="text-lg font-bold">Home</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="font-black text-transparent text-5xl bg-clip-text bg-gradient-to-br from-brand-400 to-brand-900 inline-block">
+        Tusks
+      </h1>
+      <p>not everyone has the memory of an elephant</p>
+    </div>
+  );
 };
 
 export default Home;
