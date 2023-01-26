1794ce99d2a59fd773aad3b81884f48d0eba6d60
Martin Šimeček <simecek.m@seznam.cz>
2023-01-26 21:49:50 +0100

    add brand color to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: "#B0FFD9",
+          100: "#8DFFC8",
+          200: "#77F9B8",
+          300: "#5FF2A7",
+          400: "#44EB96",
+          500: "#1DE484",
+          600: "#15BD6D",
+          700: "#0D9C57",
+          800: "#057942",
+          900: "#00592E",
+        },
+      },
+    },
   },
   plugins: [],
 };
