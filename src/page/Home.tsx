781deac62deb01e4e7927a3423db95702b85e65e
Martin Šimeček <simecek.m@seznam.cz>
2023-01-27 20:29:24 +0100

    add Button component to Home page

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,10 +1,13 @@
+import Button from "component/Button";
+
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="font-black text-transparent text-5xl bg-clip-text bg-gradient-to-br from-brand-400 to-brand-900 inline-block">
+    <div className="flex h-screen flex-col items-center justify-center p-4">
+      <h1 className="inline-block bg-gradient-to-br from-brand-400 to-brand-900 bg-clip-text text-5xl font-black text-transparent">
         Tusks
       </h1>
       <p>not everyone has the memory of an elephant</p>
+      <Button text="login" className="mt-5" />
     </div>
   );
 };
