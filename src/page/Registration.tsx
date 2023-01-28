234b7a9a683598b96ff69b021d4fcf079d73e20b
Martin Šimeček <simecek.m@seznam.cz>
2023-01-29 00:00:36 +0100

    add IconButton to Registration page

--- src/page/Registration.tsx
+++ src/page/Registration.tsx
@@ -1,15 +1,23 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import Button from "component/Button";
+import IconButton from "component/IconButton";
 import { FC } from "react";
+import { FaLongArrowAltLeft } from "react-icons/fa";
 
 const Registration: FC = () => {
   const { logout } = useAuth0();
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-xl font-bold">Registration</h1>
-      <div>First time here?</div>
-      <Button text="cancel" onClick={() => logout()} />
-    </div>
+    <>
+      <IconButton
+        text="back"
+        icon={<FaLongArrowAltLeft />}
+        className="absolute"
+        onClick={logout}
+      />
+      <div className="flex h-screen flex-col items-center justify-center">
+        <h1 className="text-xl font-bold">Registration</h1>
+        <div>First time here?</div>
+      </div>
+    </>
   );
 };
 
