78d71ebde9da91989bf1ab8cecd96ccd4a1ba9b8
Martin Šimeček <simecek.m@seznam.cz>
2023-01-28 23:54:11 +0100

    add IconButton component

---
+++ src/component/IconButton.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+
+interface IconButtonProps {
+  icon: JSX.Element;
+  text?: string;
+  className?: string;
+  onClick?: () => void;
+}
+
+const IconButton: FC<IconButtonProps> = ({
+  icon,
+  text,
+  onClick,
+  className,
+}) => {
+  return (
+    <button
+      className={`${className} text-md m-1 flex flex-row items-center justify-center gap-2 rounded-xl bg-gray-900 px-4 py-2 font-bold text-white transition duration-300 hover:bg-brand-600`}
+      onClick={onClick}
+    >
+      {icon}
+      {text}
+    </button>
+  );
+};
+
+export default IconButton;
