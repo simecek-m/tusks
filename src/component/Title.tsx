c188d486cf46980f0ed8341f437e1a2b88aa239d
Martin Šimeček <simecek.m@seznam.cz>
2023-01-29 14:09:37 +0100

    Title component use type prop

--- src/component/Title.tsx
+++ src/component/Title.tsx
@@ -1,14 +1,21 @@
 import { FC } from "react";
 
+type TitleType = "brand" | "color";
+
 interface TitleProps {
   text: string;
+  type?: TitleType;
   className?: string;
 }
 
-const Title: FC<TitleProps> = ({ text, className }) => {
+const Title: FC<TitleProps> = ({ text, type = "brand", className }) => {
   return (
     <h1
-      className={`${className} inline-block bg-gradient-to-br from-brand-400 to-brand-900 bg-clip-text text-5xl font-black text-transparent`}
+      className={`${className} ${
+        type === "brand"
+          ? "bg-gradient-to-br from-brand-400 to-brand-900 bg-clip-text text-transparent"
+          : ""
+      } inline-block text-5xl font-black`}
     >
       {text}
     </h1>
