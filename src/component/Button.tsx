738b6c7f4963664fa481237c57a3ea3b22db03d5
Martin Šimeček <simecek.m@seznam.cz>
2023-01-30 21:51:05 +0100

    add type prop to Button component
    - reset
    - button
    - submit

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,16 +1,22 @@
-import { FC } from "react";
+import { FC, ButtonHTMLAttributes } from "react";
 
 interface ButtonProps {
   text: string;
   className?: string;
-  onClick: () => void;
+  onClick?: () => void;
+  type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
 }
 
-const Button: FC<ButtonProps> = ({ text, onClick, className }) => {
+const Button: FC<ButtonProps> = ({
+  text,
+  onClick,
+  type = "button",
+  className = "",
+}) => {
   return (
     <button
       onClick={onClick}
-      type="button"
+      type={type}
       className={`${className} w-full rounded-md bg-gradient-to-br from-brand-400 to-brand-900 px-5 py-2 font-bold text-white sm:w-1/2 lg:max-w-sm`}
     >
       {text}
