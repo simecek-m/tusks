1a880d6d1e6709775dddfde303ae436340b2b82e
Martin Šimeček <simecek.m@seznam.cz>
2023-01-31 21:53:54 +0100

    add optional error prop to Input component

--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -4,6 +4,7 @@ import React, {
   HTMLInputTypeAttribute,
   LegacyRef,
 } from "react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps {
   name: string;
@@ -14,6 +15,7 @@ interface InputProps {
   className?: string;
   defaultValue?: string;
   disabled?: boolean;
+  error?: FieldError;
 }
 
 const Input = (
@@ -26,14 +28,18 @@ const Input = (
     defaultValue,
     disabled = false,
     className,
+    error,
   }: InputProps,
   ref: LegacyRef<HTMLInputElement>
 ) => {
   return (
     <div className="my-2 flex w-full flex-col">
       {label && (
-        <label htmlFor={name} className="text-sm">
-          {label}
+        <label
+          htmlFor={name}
+          className={` ${error ? `font-bold text-red-500` : ""} text-sm`}
+        >
+          {error?.message ?? label}
         </label>
       )}
       <input
