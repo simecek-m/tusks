50f7c26b93a472f8f4d0dd5e66548ba8431aa2ca
Martin Šimeček <simecek.m@seznam.cz>
2023-01-31 21:42:46 +0100

    add disable prop to Button component

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -5,19 +5,22 @@ interface ButtonProps {
   className?: string;
   onClick?: () => void;
   type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  disabled?: boolean;
 }
 
 const Button: FC<ButtonProps> = ({
   text,
   onClick,
   type = "button",
+  disabled = false,
   className = "",
 }) => {
   return (
     <button
       onClick={onClick}
       type={type}
-      className={`${className} w-full rounded-md bg-gradient-to-br from-brand-400 to-brand-900 px-5 py-2 font-bold text-white sm:w-1/2 lg:max-w-sm`}
+      disabled={disabled}
+      className={`${className} w-full rounded-md bg-gradient-to-br from-brand-400 to-brand-900 px-5 py-2 font-bold text-white disabled:from-gray-500 disabled:to-gray-500 sm:w-1/2 lg:max-w-sm`}
     >
       {text}
     </button>
