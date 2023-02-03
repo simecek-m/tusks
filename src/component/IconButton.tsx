c98c54df4fa9f8f5491f255f442084113ec62300
Martin Šimeček <simecek.m@seznam.cz>
2023-02-03 22:22:54 +0100

    migrate from react-icons to fontAwesome icons

--- src/component/IconButton.tsx
+++ src/component/IconButton.tsx
@@ -1,7 +1,9 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC } from "react";
 
 interface IconButtonProps {
-  icon: JSX.Element;
+  icon: IconProp;
   text?: string;
   className?: string;
   onClick?: () => void;
@@ -15,10 +17,10 @@ const IconButton: FC<IconButtonProps> = ({
 }) => {
   return (
     <button
-      className={`${className} text-md m-1 flex flex-row items-center justify-center gap-2 rounded-xl bg-gray-900 font-bold text-white transition duration-300 hover:bg-brand-600`}
+      className={`${className} text-md m-1 flex flex-row items-center justify-center gap-2 rounded-xl bg-gray-900  font-bold text-white transition duration-300 hover:bg-brand-600`}
       onClick={onClick}
     >
-      {icon}
+      <FontAwesomeIcon icon={icon} />
       {text}
     </button>
   );
