61cf12ac37a4538de524e69b837716d4ad9bd862
Martin Šimeček <simecek.m@seznam.cz>
2023-02-04 20:29:39 +0100

    switch to Coiny and Sora fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        brand: ["Coiny", "cursive"],
+      },
       colors: {
         brand: {
           50: "#B0FFD9",
