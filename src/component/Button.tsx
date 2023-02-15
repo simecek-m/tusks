74ec20c1b0ec3d95dbae8fef52acc945f03934ca
Martin Šimeček <simecek.m@seznam.cz>
2023-02-15 22:19:23 +0100

    implement button variants
    - primary
    - destructive

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -3,6 +3,13 @@ import clsx from "clsx";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+type ButtonVariant = "primary" | "destructive";
+
+const ButtonVariants: Record<ButtonVariant, string> = {
+  primary: "bg-brand-700",
+  destructive: "bg-red-600",
+};
+
 export interface ButtonProps {
   type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
   disabled?: boolean;
@@ -11,6 +18,7 @@ export interface ButtonProps {
   children: string;
   icon: IconProp;
   hoverIcon: IconProp;
+  variant?: ButtonVariant;
 }
 
 const Button: FC<ButtonProps> = ({
@@ -19,6 +27,7 @@ const Button: FC<ButtonProps> = ({
   disabled = false,
   isSubmitting = false,
   icon,
+  variant = "primary",
   hoverIcon,
   children,
 }) => {
@@ -29,7 +38,8 @@ const Button: FC<ButtonProps> = ({
       disabled={disabled}
       className={clsx(
         { "cursor-wait": isSubmitting },
-        "group flex w-full flex-row items-center justify-center gap-2 overflow-hidden rounded-full bg-brand-700 py-2 px-2 pr-5 font-bold text-white disabled:cursor-not-allowed disabled:opacity-40 sm:w-fit"
+        ButtonVariants[variant],
+        "group flex w-full flex-row items-center justify-center gap-2 overflow-hidden rounded-full py-2 px-2 pr-5 font-bold text-white disabled:cursor-not-allowed disabled:opacity-40 sm:w-fit"
       )}
     >
       {!disabled && (
@@ -37,13 +47,13 @@ const Button: FC<ButtonProps> = ({
           {icon && (
             <FontAwesomeIcon
               icon={icon}
-              className="h-4 w-4 translate-y-0 rounded-full bg-brand-600 p-2 text-white transition duration-500 ease-in-out group-hover:-translate-y-10"
+              className="h-4 w-4 translate-y-0 rounded-full bg-white bg-opacity-25 p-2 text-white transition duration-500 ease-in-out group-hover:-translate-y-10"
             />
           )}
           {hoverIcon && (
             <FontAwesomeIcon
               icon={hoverIcon}
-              className="absolute h-4 w-4 translate-y-10 rounded-full bg-brand-600 p-2 text-white transition duration-500 ease-in-out group-hover:translate-y-0"
+              className="absolute h-4 w-4 translate-y-10 rounded-full bg-white bg-opacity-25 p-2 text-white transition duration-500 ease-in-out group-hover:translate-y-0"
             />
           )}
         </div>
@@ -51,7 +61,7 @@ const Button: FC<ButtonProps> = ({
       {disabled && (
         <FontAwesomeIcon
           icon="xmark"
-          className="h-4 w-4 rounded-full bg-brand-600 p-2 text-white"
+          className="h-4 w-4 rounded-full bg-white bg-opacity-25 p-2 text-white"
         />
       )}
       <span className="">{isSubmitting ? "processing" : children}</span>
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,25 +1,31 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 import Button from "component/Button";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Base = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = Template.bind({});
+export const Destructive = Template.bind({});
 
-Base.args = {
+Primary.args = {
   children: "Sign In",
   disabled: false,
   isSubmitting: false,
   icon: "lock",
   hoverIcon: "key",
+  variant: "primary",
+};
+
+Destructive.args = {
+  children: "Remove",
+  disabled: false,
+  isSubmitting: false,
+  icon: "trash-alt",
+  hoverIcon: "user-xmark",
+  variant: "destructive",
 };
