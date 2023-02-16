46d0bf4101962ea4211a67abf636f39eb0c1d2d4
Martin Šimeček <simecek.m@seznam.cz>
2023-02-16 21:49:26 +0100

    encapsulate Card component

--- src/page/Registration.tsx
+++ src/page/Registration.tsx
@@ -4,7 +4,6 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError, AxiosResponse } from "axios";
 import Button from "component/button/Button";
 import Card from "component/Card";
-import IconButton from "component/IconButton";
 import Input from "component/Input";
 import Title from "component/Title";
 import { AVATAR_IMG } from "constant/assets";
@@ -57,93 +56,83 @@ const Registration: FC = () => {
   };
 
   return (
-    <div className="flex h-screen w-screen justify-center overflow-auto bg-gradient-to-br from-primary-400 to-primary-900 p-5 md:p-10">
-      <Card className="relative">
-        <>
-          <IconButton
-            onClick={() =>
-              logout({ logoutParams: { returnTo: window.location.origin } })
-            }
-            icon="close"
-            className="absolute top-0 right-0 h-10 w-10 rounded-full"
-          />
-          <div className="flex-col">
-            <div className="flex flex-col items-center">
-              <Title className="text-gray-900">Welcome</Title>
-              <p className="mb-5 text-center font-light">
-                before you continue please checkout your personal data
-              </p>
-            </div>
-            <form
-              onSubmit={handleSubmit(submit)}
-              className="flex flex-col items-end gap-5"
-            >
-              <div className="flex w-full flex-col items-center justify-center gap-5 sm:flex-row">
-                <img
-                  src={user?.picture ?? AVATAR_IMG}
-                  alt="profile picture"
-                  className="aspect-square w-1/2 rounded-3xl object-cover shadow-2xl"
-                />
-                <div className="flex w-full flex-col">
-                  <Input
-                    label="username"
-                    defaultValue={user?.family_name
-                      ?.toLocaleLowerCase()
-                      .normalize("NFD")
-                      .replace(/\p{Diacritic}/gu, "")}
-                    error={errors.username}
-                    prefix="@"
-                    ref={(e) => {
-                      ref(e);
-                      usernameInputRef.current = e;
-                    }}
-                    {...rest}
-                  />
-                  <Input
-                    label="first name"
-                    defaultValue={user?.given_name}
-                    error={errors.firstName}
-                    {...register("firstName")}
-                  />
-                  <Input
-                    label="last name"
-                    defaultValue={user?.family_name}
-                    error={errors.lastName}
-                    {...register("lastName")}
-                  />
-                  <Input
-                    label="e-mail"
-                    type="email"
-                    defaultValue={user?.email}
-                    error={errors.email}
-                    {...register("email")}
-                  />
-                  <input
-                    value={user?.sub}
-                    hidden
-                    readOnly
-                    {...register("id")}
-                  />
-                  <input
-                    value={user?.picture}
-                    hidden
-                    readOnly
-                    {...register("picture")}
-                  />
-                </div>
-              </div>
-              <Button
-                icon="arrow-right"
-                hoverIcon="check"
-                type="submit"
-                disabled={!isValid}
-                isSubmitting={isSubmitting}
-              >
-                continue
-              </Button>
-            </form>
+    <div className="flex h-screen w-screen overflow-auto bg-gradient-to-br from-primary-400 to-primary-900 p-5">
+      <Card
+        onClose={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        <div className="flex flex-col">
+          <div className="flex flex-col items-center">
+            <Title className="text-gray-900">Welcome</Title>
+            <p className="mb-5 text-center font-light">
+              before you continue please checkout your personal data
+            </p>
           </div>
-        </>
+          <form
+            onSubmit={handleSubmit(submit)}
+            className="flex flex-col items-end gap-5"
+          >
+            <div className="flex w-full flex-col items-center justify-center gap-5 sm:flex-row">
+              <img
+                src={user?.picture ?? AVATAR_IMG}
+                alt="profile picture"
+                className="aspect-square w-1/2 rounded-full object-cover shadow-2xl"
+              />
+              <div className="flex w-full flex-col">
+                <Input
+                  label="username"
+                  defaultValue={user?.family_name
+                    ?.toLocaleLowerCase()
+                    .normalize("NFD")
+                    .replace(/\p{Diacritic}/gu, "")}
+                  error={errors.username}
+                  prefix="@"
+                  ref={(e) => {
+                    ref(e);
+                    usernameInputRef.current = e;
+                  }}
+                  {...rest}
+                />
+                <Input
+                  label="first name"
+                  defaultValue={user?.given_name}
+                  error={errors.firstName}
+                  {...register("firstName")}
+                />
+                <Input
+                  label="last name"
+                  defaultValue={user?.family_name}
+                  error={errors.lastName}
+                  {...register("lastName")}
+                />
+                <Input
+                  label="e-mail"
+                  type="email"
+                  defaultValue={user?.email}
+                  error={errors.email}
+                  {...register("email")}
+                />
+                <input value={user?.sub} hidden readOnly {...register("id")} />
+                <input
+                  value={user?.picture}
+                  hidden
+                  readOnly
+                  {...register("picture")}
+                />
+              </div>
+            </div>
+            <Button
+              icon="arrow-right"
+              hoverIcon="check"
+              type="submit"
+              isDisabled={!isValid}
+              isSubmitting={isSubmitting}
+            >
+              continue
+            </Button>
+          </form>
+        </div>
       </Card>
     </div>
   );
