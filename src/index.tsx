354d36daa25440a5c953f0fb1b356c2e7527a792
Martin Šimeček <simecek.m@seznam.cz>
2023-02-25 23:31:58 +0100

    migrate CRA project to Vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
-import App from "App";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import * as serviceWorker from "serviceWorker";
+import App from "App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-root.render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
