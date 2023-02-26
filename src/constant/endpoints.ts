438d0a79f907a9a7101cf2b8448b03275f3afab2
Martin Šimeček <simecek.m@seznam.cz>
2023-02-26 20:01:33 +0100

    simplify TusksApiHook

---
+++ src/constant/endpoints.ts
@@ -0,0 +1,2 @@
+export const PROFILES_ENDPOINT = "/profiles";
+export const MY_PROFILE_ENDPOINT = `${PROFILES_ENDPOINT}/me`;
--- src/hook/api.ts
+++ src/hook/api.ts
@@ -1,35 +1,34 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { MY_PROFILE_ENDPOINT, PROFILES_ENDPOINT } from "constant/endpoints";
 import { IProfile } from "type";
 
-interface TuskApiFunctions {
+interface TusksApiFunctions {
   fetchMyProfile: () => Promise<AxiosResponse<IProfile>>;
   postRegistration: (profile: IProfile) => Promise<AxiosResponse<IProfile>>;
 }
 
-const useTusksApi = (): TuskApiFunctions => {
+const useTusksApi = (): TusksApiFunctions => {
   const { getAccessTokenSilently } = useAuth0();
 
-  const getTusksApiClient: () => Promise<AxiosInstance> = async () => {
+  const client: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_API_URL,
+  });
+
+  client.interceptors.request.use(async (config) => {
     const accessToken = await getAccessTokenSilently();
-    return axios.create({
-      baseURL: import.meta.env.VITE_BACKEND_API_URL,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-  };
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  });
 
   const fetchMyProfile = async (): Promise<AxiosResponse<IProfile>> => {
-    const client = await getTusksApiClient();
-    return client.get<IProfile>("/profiles/me");
+    return client.get<IProfile>(MY_PROFILE_ENDPOINT);
   };
 
   const postRegistration = async (
     profile: IProfile
   ): Promise<AxiosResponse<IProfile>> => {
-    const client = await getTusksApiClient();
-    return client.post<IProfile>("/profiles", profile);
+    return client.post<IProfile>(PROFILES_ENDPOINT, profile);
   };
 
   return {
