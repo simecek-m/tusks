9dbfb3f65385d197e5aaf269fe8b245bc5151bfe
Martin Šimeček <simecek.m@seznam.cz>
2023-02-27 22:35:34 +0100

    fix dark mode of Card component

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -8,9 +8,9 @@ export interface CardProps {
 
 export const Card: FC<CardProps> = ({ children, onClose }) => {
   return (
-    <div className="relative m-auto w-full max-w-4xl rounded-3xl bg-white p-10 shadow-lg dark:bg-slate-800">
+    <div className="relative m-auto w-full max-w-4xl rounded-3xl bg-white p-10 text-black shadow-lg dark:bg-slate-700 dark:text-white">
       <button
-        className="absolute top-0 right-0 m-1 flex h-10 w-10 items-center justify-center gap-2 rounded-full bg-slate-700 text-white transition duration-300 hover:bg-primary-600"
+        className="absolute top-0 right-0 m-1 flex h-10 w-10 items-center justify-center gap-2 rounded-full bg-slate-800 text-white transition duration-300 hover:bg-primary-600"
         onClick={onClose}
       >
         <FontAwesomeIcon icon="close" />
