058fa2c4d00bd79702151ab721431a915dd89369
Martin Šimeček <simecek.m@seznam.cz>
2023-02-27 22:27:53 +0100

    add darkmode to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,11 @@
 import "tailwindcss/tailwind.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { withTailwindTheme } from "./withTailwindTheme.decorator";
 
 library.add(fas);
+
+export const decorators = [withTailwindTheme];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,6 +13,21 @@ export const parameters = {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+  },
+};
+
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    toolbar: {
+      icon: "paintbrush",
+      items: [
+        { value: "light", title: "Light", left: "🌞" },
+        { value: "dark", title: "Dark", left: "🌛" },
+      ],
+      dynamicTitle: true,
     },
   },
 };
