00b63e118fd51e20f65d64423dcc6111e5c5c2e6
Martin Šimeček <simecek.m@seznam.cz>
2023-03-28 17:07:37 +0200

    topbar absolute position on bigger screens

--- src/component/layout/widget/TopBar.tsx
+++ src/component/layout/widget/TopBar.tsx
@@ -4,7 +4,7 @@ import ThemeSwitcher from "component/layout/widget/ThemeSwitcher";
 const Topbar = () => {
   return (
     <div className="flex w-full select-none justify-end md:pr-2 md:pt-2">
-      <div className="flex w-full flex-row items-center justify-end gap-2 bg-white py-2 px-3 shadow-lg dark:bg-slate-700 md:w-fit md:rounded-full">
+      <div className="right-3 top-3 flex w-full flex-row items-center justify-end gap-2 bg-white py-2 px-3 shadow-lg dark:bg-slate-700 md:absolute md:w-fit md:rounded-full">
         <ThemeSwitcher />
         <ProfileWidget />
       </div>
