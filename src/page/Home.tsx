b52b1a9f79302e6031ecdfe83b5ed0b86766d341
Martin Šimeček <simecek.m@seznam.cz>
2024-05-15 23:03:38 +0200

    redesign Home screen

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -8,6 +8,7 @@ import { motion } from "framer-motion";
 import { FC } from "react";
 import { useNavigate } from "react-router-dom";
 import PageContent from "component/layout/PageContent";
+import { TodoListPreview } from "component/todo/TodoListPreview";
 
 const Home: FC = () => {
   const { loginWithPopup, isAuthenticated } = useAuth0();
@@ -16,39 +17,55 @@ const Home: FC = () => {
   return (
     <PageLayout>
       <PageContent>
-        <div className="flex h-full flex-col items-center justify-center">
-          <motion.div
-            className="mb-5"
-            initial={{ scale: 0.8 }}
-            whileHover={{ scale: 1 }}
-            transition={{ type: "spring", bounce: 0.5 }}
-          >
-            <Logo height="20vh" width="100%" />
-          </motion.div>
-          <Title>Tusks</Title>
-          <p>not everyone has the memory of an elephant</p>
-          <div className="mt-10 flex w-full flex-col items-center">
-            {isAuthenticated ? (
-              <Button
-                icon="right-long"
-                hoverIcon="door-open"
-                onClick={() => navigate(INDEX_PATH)}
+        <div className="flex h-full w-full flex-col items-center justify-around md:flex-row">
+          <div className="flex w-full flex-col items-center justify-center">
+            <Title>Tusks</Title>
+            <p>not everyone has the memory of an elephant</p>
+            <div className="mt-10 flex w-full flex-col items-center">
+              <motion.div
+                className="mb-5"
+                initial={{ scale: 0.8 }}
+                whileHover={{ scale: 1 }}
+                transition={{ type: "spring", bounce: 0.5 }}
               >
-                Continue
-              </Button>
-            ) : (
-              <Button
-                icon="lock"
-                hoverIcon="key"
-                onClick={() =>
-                  loginWithPopup({
-                    authorizationParams: { prompt: "select_account" },
-                  })
-                }
-              >
-                Sign In
-              </Button>
-            )}
+                <Logo height="20vh" width="100%" />
+              </motion.div>
+            </div>
+          </div>
+          <div className="flex w-full justify-center">
+            <div className="flex w-1/2 min-w-[300px] flex-col items-end gap-4">
+              <TodoListPreview
+                title="Shop"
+                todos={[
+                  { isCompleted: false, label: "Eggs" },
+                  { isCompleted: true, label: "Milk" },
+                  { isCompleted: false, label: "Vegetables" },
+                ]}
+              />
+              <div>
+                {isAuthenticated ? (
+                  <Button
+                    icon="right-long"
+                    hoverIcon="door-open"
+                    onClick={() => navigate(INDEX_PATH)}
+                  >
+                    Continue
+                  </Button>
+                ) : (
+                  <Button
+                    icon="lock"
+                    hoverIcon="key"
+                    onClick={() =>
+                      loginWithPopup({
+                        authorizationParams: { prompt: "select_account" },
+                      })
+                    }
+                  >
+                    Sign In
+                  </Button>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </PageContent>
